b1b07c4294d9b5aec1685aa94f8ed59bff2ed780
Alex Williams <alexanderjdwilliams@gmail.com>
2017-05-14 22:29:47 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// We need to grab multiple user input values, specfically for second(s), minute(s), or hour(s)
+	var secondVal = document.getElementById('seconds').value;
+	var minuteVal = document.getElementById('minutes').value;
+	var hourVal = document.getElementById('hours').value;
+
+// RegEx of only numbers, no strings or character values
+
+// Once we have the user values, we need to put those into a type of countdown function 
+
+// That variable will be placed into a function that decrements it by the second
