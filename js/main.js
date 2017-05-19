2855fc678025c3c9804c3ac30b5c069350fc9b9a
Alex Williams <alexanderjdwilliams@gmail.com>
2017-05-19 02:48:22 -0700

    added second countdown functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 // We need to grab multiple user input values, specfically for second(s), minute(s), or hour(s)
-	var secondVal = document.getElementById('seconds').value;
-	var minuteVal = document.getElementById('minutes').value;
-	var hourVal = document.getElementById('hours').value;
-
-// RegEx of only numbers, no strings or character values
-
 // Once we have the user values, we need to put those into a type of countdown function 
 
+
+
+
+   function startItUp(){
+       var secondsy = document.getElementById('seconds').value;
+       seconds = parseInt(secondsy);
+      var countdownTimer = setInterval('secondPassed()', 1000);
+// alert(seconds);
+}
+
+function secondPassed() {
+
+    var minutes = Math.round((seconds - 30)/60);
+    var remainingSeconds = seconds % 60;
+    
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;  
+    }
+    document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
+    if (seconds == 0) {
+        clearInterval(countdownTimer);
+        document.getElementById('alert').innerHTML = "Buzz Buzz";
+    } else {
+        seconds = seconds - 1;
+    }
+}
+
+
 // That variable will be placed into a function that decrements it by the second
+
+
+// 
