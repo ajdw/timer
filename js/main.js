aae8170a925371ba0564b818dc082827965065d0
Alex Williams <alexanderjdwilliams@gmail.com>
2017-05-19 02:51:16 -0700

    fixed formatting

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 // We need to grab multiple user input values, specfically for second(s), minute(s), or hour(s)
 // Once we have the user values, we need to put those into a type of countdown function 
 
-
-
-
    function startItUp(){
        var secondsy = document.getElementById('seconds').value;
        seconds = parseInt(secondsy);
@@ -27,9 +24,3 @@ function secondPassed() {
         seconds = seconds - 1;
     }
 }
-
-
-// That variable will be placed into a function that decrements it by the second
-
-
-// 
