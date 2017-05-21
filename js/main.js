79c595ec8bdfa6a041da46bc905bcf3b7359538c
Alex Williams <alexanderjdwilliams@gmail.com>
2017-05-20 23:00:12 -0700

    fixed formatting

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 // We need to grab multiple user input values, specfically for second(s), minute(s), or hour(s)
-// Once we have the user values, we need to put those into a type of countdown function 
+// Once we have the user values, we need to put those into a type of countdown function
 
-   function startItUp(){
-       var secondsy = document.getElementById('seconds').value;
-       seconds = parseInt(secondsy);
-      var countdownTimer = setInterval('secondPassed()', 1000);
-// alert(seconds);
+function startItUp(){
+    var secondsInput = document.getElementById('seconds').value;
+    seconds = parseInt(secondsInput);
+    var countdownTimer = setInterval('secondPassed()', 1000);
 }
+
+	var numIntiate = 1;
 
 function secondPassed() {
 
@@ -14,13 +15,14 @@ function secondPassed() {
     var remainingSeconds = seconds % 60;
     
     if (remainingSeconds < 10) {
-        remainingSeconds = "0" + remainingSeconds;  
+        remainingSeconds = '0' + remainingSeconds;  
     }
-    document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
+    document.getElementById('countdown').innerHTML = minutes + ':' + remainingSeconds;
     if (seconds == 0) {
-        clearInterval(countdownTimer);
-        document.getElementById('alert').innerHTML = "Buzz Buzz";
+        clearInterval(secondPassed);
+        document.getElementById('alert').innerHTML = 'Buzz Buzz';
+        seconds = 0;
     } else {
-        seconds = seconds - 1;
+        seconds--;
     }
 }
